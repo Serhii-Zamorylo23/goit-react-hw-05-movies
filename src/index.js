21a18d2b8cd77ab1092c84021828f8d84a1ffd56
Serhii-Zamorylo23 <zamorylo@i.ua>
2025-08-24 12:43:12 +0300

    deploy

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/movies'>
+    <BrowserRouter basename='/goit-react-hw-05-movies'>
       <App/>
     </BrowserRouter>
   </React.StrictMode>
